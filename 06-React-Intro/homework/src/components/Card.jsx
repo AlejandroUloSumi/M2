7387987cd15cf35c1b6491ac434f5e61d4a42df5
Alejandro Jaime Ulo Sumi <alejandrojus2@gmail.com>
2022-07-02 16:53:15 -0300

    first commit

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,17 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+  <div>
+    <button onClick={props.onClose}>X</button>
+    <h4>{props.name}</h4>
+    <div>
+      <p>Min</p>
+      <p>{props.min}</p>
+      <p>Max</p>
+      <p>{props.max}</p>
+    </div>
+  <img src="https://cdn.icon-icons.com/icons2/472/PNG/48/dinosaur-48_44802.png" alt={"img no cargada"}/>
+  </div>
+  )
 };
